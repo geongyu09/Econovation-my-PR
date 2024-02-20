8af4796e3c41be60897fe5ef2c8445063bfee9a0
박건규 <geongyu09@gmail.com>
2024-02-20 19:42:55 +0900

    feat: 화면 축소시 margin값 수정

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -3,7 +3,7 @@ import Content from "./common/Content";
 
 export default function ContentSection() {
   return (
-    <section className="md:w-8/12 w-11/12 mx-auto mt-10 grid md:grid-cols-2 gap-3">
+    <section className="md:w-8/12 w-10/12 mx-auto mt-10 grid md:grid-cols-2 gap-3">
       {contentData.map((content) => (
         <Content key={content.title} {...content} />
       ))}
