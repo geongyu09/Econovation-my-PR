dcfcac265e818a7890f2127512c70e776673c5db
박건규 <geongyu09@gmail.com>
2024-02-23 15:42:10 +0900

    feat: add analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ContentSection from "@/components/ContentSection";
 import HeaderSection from "@/components/HeaderSection";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <main className="">
+      <Analytics />
       <HeaderSection />
       <ContentSection />
     </main>
